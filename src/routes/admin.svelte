4b29ffe166e2a05c69ee911d81b0685f7cc3dd04
4allofus <33950526+4allofus@users.noreply.github.com>
2021-11-12 03:50:33 +0700

    Admin fix 9

--- src/routes/admin.svelte
+++ src/routes/admin.svelte
@@ -1,11 +1,12 @@
 <script>
       import 'papercss/dist/paper.min.css'
-      import { Collapsible, Table } from 'spaper';
+      import "Collapsible, Table" from 'spaper';
 
       const supplies = [
 		    { id: '1', title: 'Rumah pik murah', harga: '5', satuan: 'M', detil: 'Jl Camar 5 no 32', kategori: 'rumah' },
 		    { id: '2', title: 'Rumah pik murah', harga: '5', satuan: 'M', detil: 'Jl Camar 5 no 32', kategori: 'rumah' }
 	    ];
+
 </script>
 
 
