1e0ef391432a88edbc39fe2e06c25f20782ab183
4allofus <33950526+4allofus@users.noreply.github.com>
2021-11-12 15:42:02 +0700

    Admin fix 1

--- src/routes/admin.svelte
+++ src/routes/admin.svelte
@@ -4,10 +4,11 @@
       import {Table} from 'spaper';
 
       const data = [
-        { rank: 1, name: 'Novak Djokovic', country: 'Serbia' },
-        { rank: 2, name: 'Daniil Medvedev', country: 'Russia' },
-        { rank: 3, name: 'Stefanos Tsitsipas', country: 'Greece' },
-        { rank: 4, name: 'Alexander Zverev', country: 'Germany' },
+        { no: 1, title: 'Rumah pik', harga: 5, satuan: 'M', 
+          detil: 'Jalan besar, hook', kategori: 'rumah' },
+        { no: 2, title: 'Rumah pik', harga: 5, satuan: 'M', 
+          detil: 'Jalan besar, hook', kategori: 'rumah' },
+        
       ];
 </script>
 
@@ -15,5 +16,5 @@
 <div class="paper container-lg">  
 
   <Collapsible label="Add New Listing">Collapsible : Add New Listing</Collapsible>
-  <Table {data} />
+  <Table {data} hoverable/>
 </div>  
