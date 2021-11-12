03ffc761ab45901b125c645c34cedc535a701125
4allofus <33950526+4allofus@users.noreply.github.com>
2021-11-12 15:59:37 +0700

    admin fix 5

--- src/routes/admin.svelte
+++ src/routes/admin.svelte
@@ -1,12 +1,18 @@
 <script>
       import 'papercss/dist/paper.min.css'
-      import {Collapsible} from 'spaper';
-      import {Table} from 'spaper';
+      import {Collapsible, Table, Input} from 'spaper';
+      //import {Table} from 'spaper';
+
+      let title = '';
+      let harga = 0;
+      let satuan = '';
+      let detil = '';
+      let kategori = '';
 
       let data = [
-        { no: 1, title: 'Rumah pik', harga: 5, satuan: 'M', 
+        { title: 'Rumah pik', harga: 5, satuan: 'M', 
           detil: 'Jalan besar, hook', kategori: 'rumah' },
-        { no: 2, title: 'Apartmen pik', harga: 700, satuan: 'Jt', 
+        { title: 'Apartmen pik', harga: 700, satuan: 'Jt', 
           detil: 'Lantai 7 view laut', kategori: 'apartmen' },
         
       ];
@@ -15,6 +21,13 @@
 
 <div class="paper container-lg">  
 
-  <Collapsible label="Add New Listing">Collapsible : Add New Listing</Collapsible>
+  <Collapsible label="Add New Listing">
+
+    <div class="form-group">
+      <Input placeholder="Judul" block bind={title}/>
+    </div>
+    Listing : {title}
+  
+  </Collapsible>
   <Table {data} hoverable/>
 </div>  
