4b9a27eb3581da71042abc5ab6feb1a95b473cbc
4allofus <33950526+4allofus@users.noreply.github.com>
2021-11-12 15:36:50 +0700

    admin fix 7

--- src/routes/admin.svelte
+++ src/routes/admin.svelte
@@ -5,7 +5,7 @@
 
       let supplies = [
 		    { id: 1, title: 'Rumah pik murah', harga: 5, satuan: 'M', detil: 'Jl Camar 5 no 32', kategori: 'rumah' },
-		    { id: 2, title: 'Rumah pik murah', harga: 5, satuan: 'M', detil: 'Jl Camar 5 no 32', kategori: 'rumah' },
+		    { id: 2, title: 'Apartment murah', harga: 700, satuan: 'Jt', detil: 'Jl Camar 5 no 32', kategori: 'apartmen' },
 	    ];
 
 </script>
