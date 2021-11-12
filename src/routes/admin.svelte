8efe63b29ff3933bfc415d45d725d3dff428cea7
4allofus <33950526+4allofus@users.noreply.github.com>
2021-11-12 15:38:11 +0700

    admin fix 8

--- src/routes/admin.svelte
+++ src/routes/admin.svelte
@@ -3,16 +3,17 @@
       import {Collapsible} from 'spaper';
       import {Table} from 'spaper';
 
-      let supplies = [
-		    { id: 1, title: 'Rumah pik murah', harga: 5, satuan: 'M', detil: 'Jl Camar 5 no 32', kategori: 'rumah' },
-		    { id: 2, title: 'Apartment murah', harga: 700, satuan: 'Jt', detil: 'Jl Camar 5 no 32', kategori: 'apartmen' },
-	    ];
-
+      const data = [
+        { rank: 1, name: 'Novak Djokovic', country: 'Serbia' },
+        { rank: 2, name: 'Daniil Medvedev', country: 'Russia' },
+        { rank: 3, name: 'Stefanos Tsitsipas', country: 'Greece' },
+        { rank: 4, name: 'Alexander Zverev', country: 'Germany' },
+      ];
 </script>
 
 
 <div class="paper container-lg">  
 
   <Collapsible label="Add New Listing">Collapsible : Add New Listing</Collapsible>
-  <Table {supplies} />
+  <Table {data} />
 </div>  
