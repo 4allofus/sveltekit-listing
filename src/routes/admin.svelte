fa658976bf7ad5dbb9b1e6a0c2c8c70ed9ff6abb
4allofus <33950526+4allofus@users.noreply.github.com>
2021-11-12 15:44:45 +0700

    Admin fix 3

--- src/routes/admin.svelte
+++ src/routes/admin.svelte
@@ -6,8 +6,8 @@
       let data = [
         { no: 1, title: 'Rumah pik', harga: 5, satuan: 'M', 
           detil: 'Jalan besar, hook', kategori: 'rumah' },
-        { no: 2, title: 'Rumah pik', harga: 5, satuan: 'M', 
-          detil: 'Jalan besar, hook', kategori: 'rumah' },
+        { no: 2, title: 'Apartmen pik', harga: 700, satuan: 'Jt', 
+          detil: 'Lantai 7 view laut', kategori: 'apartmen' },
         
       ];
 </script>
