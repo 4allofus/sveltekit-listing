4fb2a63970a6d38659212f01797ea7503abcebfd
4allofus <33950526+4allofus@users.noreply.github.com>
2021-11-15 19:46:32 +0700

    firebase lib fix

--- src/lib/firebaseConfig.js
+++ src/lib/firebaseConfig.js
@@ -1,4 +1,5 @@
 export const firebaseConfig = {
+
     apiKey: "AIzaSyAtFQ1idN8EfWztKDY9IpfUfk5CXvuO2G8",
     authDomain: "allofussvelte.firebaseapp.com",
     databaseURL: "https://allofussvelte-default-rtdb.asia-southeast1.firebasedatabase.app",
@@ -7,4 +8,5 @@ export const firebaseConfig = {
     messagingSenderId: "352722336552",
     appId: "1:352722336552:web:5e144ce988d22e799f2e1d",
     measurementId: "G-39RZPFYC16"
+    
   };
