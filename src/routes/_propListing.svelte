5940434775f41c34ffeb686a88197246a075d383
4allofus <33950526+4allofus@users.noreply.github.com>
2021-11-21 17:56:23 +0700

    propListing fix 2

--- src/routes/_propListing.svelte
+++ src/routes/_propListing.svelte
@@ -1,20 +1,19 @@
 <script>
-      export let propTitle = "..."
-      export let propKeterangan = "..."
-      export let propHarga = 1
-      export let propSatuan = "Milyar"
-      export let propKategori = "..."
-      export let propHotList = false
+      export let propTitle = "...";
+      export let propKeterangan = "...";
+      export let propHarga = 1;
+      export let propSatuan = "Milyar";
+      export let propKategori = "...";
+      export let propHotList = false;
       export let propImage = "http://kioskarikatur.files.wordpress.com/2009/02/diskon-30.jpg"
       export let web = "empty";
-      
 </script>
 
 <div class="card">
     <img src="https://picsum.photos/768" alt="Card example image">
   
     <div class="card-body">
-        {#if propHotlist == true}
+        {#if propHotlist === true}
             <h4 class="card-title">🔥🔥🔥{propTitle}🔥🔥🔥</h4>
         {:else}
             <h4 class="card-title">{propTitle}</h4>
