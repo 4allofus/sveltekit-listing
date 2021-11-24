7dbe11abc01874ca99a6f0a657b029b306ecc274
4allofus <33950526+4allofus@users.noreply.github.com>
2021-11-24 17:58:11 +0700

    timeStamp

--- src/routes/admin.svelte
+++ src/routes/admin.svelte
@@ -29,7 +29,7 @@
                 display: userIn.displayName,
                 name: userIn.email,
                 phone: userIn.phoneNumber,
-                time: serverTimestamp()
+                time: Date().getTime()
               }); 
         }
 
