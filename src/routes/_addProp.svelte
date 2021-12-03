c49c1878287027d6e6cbe7da1ec37d6d5e6e4554
4allofus <33950526+4allofus@users.noreply.github.com>
2021-12-03 12:10:54 +0700

    filename

--- src/routes/_addProp.svelte
+++ src/routes/_addProp.svelte
@@ -82,8 +82,6 @@
           </div>
           <div class="sm-7 md-7 lg-7 col">
             <input type="file" bind:filename>
-            <p>filename : {filename}</p>
-            <img src={filename} alt="Random Unsplash">
           </div>
           <div class="sm-7 md-7 lg-7 col">
             <Button type="secondary" on:click={submitForm}>Submit</Button>
