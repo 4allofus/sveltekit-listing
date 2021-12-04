0f929180f9afe07245abc4c7b6f266f8281042d7
4allofus <33950526+4allofus@users.noreply.github.com>
2021-12-04 15:03:37 +0700

    map

--- src/routes/map.svelte
+++ src/routes/map.svelte
@@ -1,44 +1,24 @@
-<script defer async
-	src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBWoZVhsZoSFSNG_fcyioe3ef5kzeuuqPE&callback=initMap&v=weekly">
-    import 'papercss/dist/paper.min.css'
-    import { Alert, Button, Input } from 'spaper';
-    import { mapConfig } from "$lib/firebaseConfig";
-    import { onMount } from 'svelte';
-
-    onMount(async () => {
-		map = new google.maps.Map(container, {
-            zoom,
-			center,
-			styles: mapStyles // optional
-		});
-	});
-
-    /* function initMap() {
-    // The location of Uluru
-    const uluru = { lat: -25.344, lng: 131.036 };
-    // The map, centered at Uluru
-    const map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 8,
-      center: uluru,
-    });
-    // The marker, positioned at Uluru
-    const marker = new google.maps.Marker({
-      position: uluru,
-      map: map,
-    });
-    } */
+<script>
+   let container;
+   let map;
+   let zoom = 8;
+   let center = {lat: -34.397, lng: 150.644};
+   
+   import { onMount } from 'svelte';
+   
+   onMount(async () => {
+       map = new google.maps.Map(container, {
+           zoom,
+           center,
+       });
+   });
 </script>
 
-
-<div class="paper container-lg">
-  <div id="map"></div>
-  </div>
-
-  <style>
-    #map {
-  height: 400px;
-  /* The height is 400 pixels */
-  width: 100%;
-  /* The width is the width of the web page */
+<style>
+.full-screen {
+   width: 100vw;
+   height: 100vh;
 }
-  </style>
+</style>
+
+<div class="full-screen" bind:this={container}></div>
---
+++ src/routes/mapContainer.svelte
@@ -0,0 +1,13 @@
+<script defer async
+	src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBWoZVhsZoSFSNG_fcyioe3ef5kzeuuqPE&callback=initMap&v=weekly">
+    import 'papercss/dist/paper.min.css'
+    import { Alert, Button, Input } from 'spaper';
+    import { mapConfig } from "$lib/firebaseConfig";
+    import Map from './Map.svelte';
+
+</script>
+
+
+<div class="paper container-lg">
+  <Map/>
+  </div>
