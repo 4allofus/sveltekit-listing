0b35f64b4ea3a0bf42a7ca425cba7bb02a177826
4allofus <33950526+4allofus@users.noreply.github.com>
2021-12-04 15:12:11 +0700

    map

--- src/routes/mapContainer.svelte
+++ src/routes/mapContainer.svelte
@@ -1,13 +1,25 @@
-<script defer async
-	src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBWoZVhsZoSFSNG_fcyioe3ef5kzeuuqPE&callback=initMap&v=weekly">
-    import 'papercss/dist/paper.min.css'
-    import { Alert, Button, Input } from 'spaper';
-    import { mapConfig } from "$lib/firebaseConfig";
-    import Map from './map.svelte';
+<script context="module">
 
+  export const load = async ({  }) =>{
+        src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBWoZVhsZoSFSNG_fcyioe3ef5kzeuuqPE&callback=initMap&v=weekly"
+        const ready = true;
+      return{
+        props: {
+          ready
+        },
+      };
+    };
 </script>
 
+<script>
+    export let ready; 
+    import 'papercss/dist/paper.min.css'
+    import { mapConfig } from "$lib/firebaseConfig";
+    import Map from './map.svelte';
+</script>
 
 <div class="paper container-lg">
+  {#if ready}
   <Map/>
-  </div>
+  {/if}
+</div>
