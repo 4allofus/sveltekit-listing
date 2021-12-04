e551c596e2d7426f060dfe57f017b9cef07cffee
4allofus <33950526+4allofus@users.noreply.github.com>
2021-12-04 20:09:11 +0700

    err

--- src/routes/map.svelte
+++ src/routes/map.svelte
@@ -7,11 +7,15 @@
    import { mapConfig, isReady} from "$lib/firebaseConfig";
    import { browser } from "$app/env";
 
+   try{
     console.log("enter loader");
-   const loader = browser && new google.maps.plugins.loader.Loader({
+    const loader = browser && new google.maps.plugins.loader.Loader({
     apiKey: "AIzaSyBWoZVhsZoSFSNG_fcyioe3ef5kzeuuqPE",
     version: "weekly",
     });
+    } catch(e){
+        console.log("catch error....");
+    }
     console.log("enter google maps");
     /* loader.load().then(() => {
         map = browser &&  new google.maps.Map(container, {
