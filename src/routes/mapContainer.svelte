2218ef9da2d20c75a5a5f24492a5e848880ec716
4allofus <33950526+4allofus@users.noreply.github.com>
2021-12-06 11:15:42 +0700

    .reset

--- src/routes/mapContainer.svelte
+++ src/routes/mapContainer.svelte
@@ -2,6 +2,8 @@
     import 'papercss/dist/paper.min.css'
     import { mapConfig, isReady } from "$lib/firebaseConfig";
     import Map from './map.svelte';
+    import { Reset } from 'styled-reset'
+
 
     let isReady_value = false;
 
